57c230125ac093b83cb62028399e4c25a58e70f3
mattia <mattia@DESKTOP-TSS6NL7>
2020-05-09 18:03:47 +0200

    Fxml aggiornato

--- src/main/java/it/polito/tdp/borders/FXMLController.java
+++ src/main/java/it/polito/tdp/borders/FXMLController.java
@@ -1,4 +1,3 @@
-
 package it.polito.tdp.borders;
 
 import java.net.URL;
@@ -7,38 +6,54 @@ import java.util.ResourceBundle;
 import it.polito.tdp.borders.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
-
-	private Model model;
 	
-    @FXML // ResourceBundle that was given to the FXMLLoader
+	private Model model;
+
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // fx:id="txtAnno"
-    private TextField txtAnno; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtAnno;
 
-    @FXML // fx:id="txtResult"
-    private TextArea txtResult; // Value injected by FXMLLoader
+    @FXML
+    private ComboBox<?> comboBox;
+
+    @FXML
+    private Button btnTrovaVicini;
+
+    @FXML
+    private TextArea txtResult;
 
     @FXML
     void doCalcolaConfini(ActionEvent event) {
 
     }
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    void doTrovaVicini(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
         assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert comboBox != null : "fx:id=\"comboBox\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnTrovaVicini != null : "fx:id=\"btnTrovaVicini\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
     
     public void setModel(Model model) {
-    	this.model = model;
+    	
+    	this.model= model;
     }
 }
