06f231beca2783471d0aeae3487e38427ee97ca8
mattia <mattia@DESKTOP-TSS6NL7>
2020-05-09 18:05:39 +0200

    Agg

--- src/main/java/it/polito/tdp/borders/EntryPoint.java
+++ src/main/java/it/polito/tdp/borders/EntryPoint.java
@@ -14,6 +14,7 @@ public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+    	
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
     	Parent root = loader.load();
         Scene scene = new Scene(root);
